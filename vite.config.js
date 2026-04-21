0ed8def989dab071386d9d73364d53719f65cd0c
jacomerodrigo33-a11y <jacomerodrigo33@gmail.com>
2026-04-21 13:01:23 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 3000,
-    allowedHosts: ['all']
+    port: parseInt(process.env.PORT) || 3000,
+    allowedHosts: 'all'
+  },
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: 'all'
   }
 })
